56fc2eedd310c09be5c2158ee20f84f1de7b6593
turtledude16 <turtledude1600@gmail.com>
2023-03-13 16:15:06 -0500

    Added print statment to print selected mode

--- Calculator.py
+++ Calculator.py
@@ -8,39 +8,83 @@ while True:
     else:
         print("Error please choose an input from the following")
 
-#int1
-while True:
-    try:
-        int1 = int(input("Please enter a number: "))
-        break
-    except:
-        print("error")
-
-#int2
-while True:
-    try:
-        int2 = int(input("Please enter another number: "))
-        break
-    except:
-        print("error")
-
 #Addition
 if type.startswith('a'):
+    print("You have selected addition")
+    #int1
+    while True:
+        try:
+            int1 = int(input("Please enter a number: "))
+            break
+        except:
+            print("error")
+    #int2
+    while True:
+        try:
+            int2 = int(input("Please enter another number: "))
+            break
+        except:
+            print("error") 
     print(f"{int1} + {int2} = ",end="")
     print(int(int1) + int(int2))
 
 #Subtraction
 elif type.startswith('s'):
+    print("You have selected subtraction")
+    #int1
+    while True:
+        try:
+            int1 = int(input("Please enter a number: "))
+            break
+        except:
+            print("error")
+    #int2
+    while True:
+        try:
+            int2 = int(input("Please enter another number: "))
+            break
+        except:
+            print("error") 
     print(f"{int1} - {int2} = ",end="")
     print(int(int1) - int(int2))
 
 #Multiplication
 elif type.startswith('m'):
+    print("You have selected multiplication")
+    #int1
+    while True:
+        try:
+            int1 = int(input("Please enter a number: "))
+            break
+        except:
+            print("error")
+    #int2
+    while True:
+        try:
+            int2 = int(input("Please enter another number: "))
+            break
+        except:
+            print("error") 
     print(f"{int1} * {int2} = ",end="")
     print(int(int1) * int(int2))
 
 #Division
 elif type.startswith('d'):
+    print("You have selected division")
+    #int1
+    while True:
+        try:
+            int1 = int(input("Please enter a number: "))
+            break
+        except:
+            print("error")
+    #int2
+    while True:
+        try:
+            int2 = int(input("Please enter another number: "))
+            break
+        except:
+            print("error") 
     print(f"{int1} / {int2} = ",end="")
     print(int(int1) / int(int2))
 else:
